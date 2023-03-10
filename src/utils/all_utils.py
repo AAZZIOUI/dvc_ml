9499b7bc4b8f07f982ce53924831149e592d5077
aazzioui <anas.azzioui@gmail.com>
2023-03-10 12:26:32 +0100

    Stage02 added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+#import pandas as pd
 
 def read_yaml(path_to_yaml: str) -> dict :
     with open(path_to_yaml) as yaml_file:
@@ -10,3 +11,13 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         print(f"directory created at {dir_path}")
+
+def save_local_df(df,df_path, index_status=False):
+    """
+    We use this function to store train and test data
+    args:
+    df: is the dataframe we want to save/store
+    df_path: is the path where we want to store the dataframe df
+    """
+    df.to_csv(df_path,index=index_status)
+    print(f"data is saved at: {df_path}")
